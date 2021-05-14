d57513df2127eb46ece76ae9f4aa1e7abfebfd8d
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2021-05-14 10:34:23 +0200

    chore: only support node 14 (#22)

--- apps/core-web-vitals/src/app/shared/shared.module.ts
+++ apps/core-web-vitals/src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
--- libs/builders/src/favicons/create-favicon.ts
+++ libs/builders/src/favicons/create-favicon.ts
@@ -2,8 +2,8 @@ import path from 'path';
 
 import { BuilderContext } from '@angular-devkit/architect';
 import fs from 'fs-extra';
-import imagemin from 'imagemin';
 import imageminPngquant from 'imagemin-pngquant';
+import imagemin from 'imagemin';
 import jimp from 'jimp';
 import sharp from 'sharp';
 import { optimize } from 'svgo';
