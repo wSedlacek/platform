159e0587111e21bedd8d67c8326992e0bd7afe04
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2021-07-20 08:49:29 +0200

    feat(image): :sparkles: init project (#104)

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   projects: [
-    '<rootDir>/apps/core-web-vitals',
+    '<rootDir>/apps/core-web-vitals-app',
     '<rootDir>/libs/builders',
     '<rootDir>/libs/semantic-release-angular',
     '<rootDir>/libs/image-optimizer',
+    '<rootDir>/libs/image',
   ],
   coverageReporters: ['html'],
 };
---
+++ libs/image/README.md
@@ -0,0 +1,1 @@
+# @ng-easy/image (WIP)
