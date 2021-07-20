b81b9d67b344c8468053b570e165b9bbf4461e66
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2021-07-20 08:27:55 +0200

    chore: :recycle: rename app projects (#103)

--- apps/core-web-vitals-app/jest.config.js
+++ apps/core-web-vitals-app/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'core-web-vitals',
+  displayName: 'core-web-vitals-app',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ module.exports = {
       tsconfig: '<rootDir>/tsconfig.spec.json',
     },
   },
-  coverageDirectory: '../../coverage/apps/core-web-vitals',
+  coverageDirectory: '../../coverage/apps/core-web-vitals-app',
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
