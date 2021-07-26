0e57fdab0e44ea798781e9686104ec699e182898
Samuel Fernández <samuelfernandez@users.noreply.github.com>
2021-07-26 09:40:49 +0200

    build: :hammer: opt in to webpack 5 for Nx (#116)

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ const { getJestProjects } = require('@nrwl/jest');
 
 module.exports = {
   coverageReporters: ['html'],
-  projects: [...getJestProjects(), '<rootDir>/libs/semantic-release-angular'],
+  projects: [...getJestProjects()],
 };
